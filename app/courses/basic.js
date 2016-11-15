94216488f1d73cd9b717d7cdbc34fa7180498e42
lorderikir <lorderikir@github.com>
2016-11-15 20:29:44 +1100

    update to handle API search method

--- app/courses/basic.js
+++ app/courses/basic.js
@@ -10,9 +10,41 @@ exports.findAll = function(req, res) {
 };
 
 exports.findById = function(req, res) {
-    res.send({id:req.params.id, name: "The Name", description: "description"});
+    var targetCourse = req.params.id;
+    var result = findUnitCode(targetCourse, JSONHandler('./app/courses/db.json'))
+
+    if(result !== false){
+      res.send({id:req.params.id, name: result.UnitName, faculty: result.Faculty});
+    } else {
+      res.send('Missing')
+    }
 };
 
 exports.test = function(req, res) {
-    res.send(JSONHandler('./app/courses/db.json'));
+    res.send();
 };
+
+//asume list is already sorted, use binary search
+function findUnitCode(target, array) {
+  var lowerBound = 0;
+  var upperBound = (array.length - 1);
+  var currentbound;
+
+  while (lowerBound <= upperBound){
+    var middle = Math.floor((lowerBound + upperBound)/2) //integer dicision to find middle
+    if(target === array[middle].UnitCode){
+      var unitName = array[middle].UnitName
+      var unitFac = array[middle].Faculty
+
+      var returnObject = {"UnitName": unitName,"Faculty": unitFac}
+      return returnObject
+    } else {
+      if(target < array[middle].UnitCode) {
+        upperBound = middle - 1
+      } else {
+        lowerBound = middle + 1
+      }
+    }
+  }
+  return false
+}

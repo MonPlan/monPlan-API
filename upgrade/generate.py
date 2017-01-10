d87e21c6a7062e405d1b72c15cb9cc43057f3388
lorderikir <ericjiang97@gmail.com>
2017-01-10 12:05:08 +1100

    push out new deploy

--- upgrade/generate.py
+++ upgrade/generate.py
@@ -6,18 +6,28 @@ import json
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://api.monplan.tech', 27017)
-db = client['unitRating']
+db = client['unitsDatabase']
 collection = db['units']
 
-input_file = open("base.json", "r")
+input_file = open("units.json", "r")
 
 data = json.loads(input_file.read())
-unitCodes = []
-for item in range(0, len(data)):
-    unitCodes.append(data[item]["UnitCode"])
 
-for units in unitCodes:
-    output = {"unitCode": units, "learnRating": 3, "enjoyRating": 3}
-    result = collection.insert_one(output)
-    result
-    print(units)
+for item in range(0, len(data)):
+    unitCode = data[item]["UnitCode"]
+    print(unitCode)
+    UnitName = data[item]["UnitName"]
+    LocationAndTime = data[item]["LocationAndTime"]
+    CreditPoints = data[item]["CreditPoints"]
+    Faculty = data[item]["Faculty"]
+    EFTSL = data[item]["EFTSL"]
+    Preqs = data[item]["Preqs"]
+    Proh = data[item]["Proh"]
+    SCABand = data[item]["SCABand"]
+    Sypnosis = data[item]["Sypnosis"]
+
+    output =  {"UnitCode": unitCode,"UnitName": UnitName,"Faculty": Faculty,"LocationAndTime": LocationAndTime,"CreditPoints": CreditPoints,"EFTSL": EFTSL,"Preqs": Preqs,"Proh": Proh,"SCABand": SCABand,"Sypnosis": Sypnosis, "enjoyRating": 3, "learnRating": 3}
+
+    collection.insert_one(output)
+    percentageComp = round(item/len(data)*100,2)
+    print(str(percentageComp)+"% Completed.")
